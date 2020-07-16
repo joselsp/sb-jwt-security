83ab031e358463520ae51cd05aac167ca958bbff
Jose Luis Solis <solis.jl@gmail.com>
2020-07-16 04:41:33 -0700

    Added Security Config

---
+++ src/main/java/com/keepcoding/jwtsecurity/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.keepcoding.jwtsecurity.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.keepcoding.jwtsecurity.filter.JwtFilter;
+import com.keepcoding.jwtsecurity.service.CustomUserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	private JwtFilter filter;
+	
+	@Autowired
+	private CustomUserDetailsService customUserDetailsService;
+	
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(customUserDetailsService);
+	}
+	
+	@Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+	
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors()
+				.and()
+				.csrf()
+				.disable()
+				.authorizeRequests()
+				.antMatchers("/authenticate", "/sign-up")
+				.permitAll()
+				.antMatchers(HttpMethod.OPTIONS, "/**")
+				.permitAll()
+				.anyRequest()
+				.authenticated()
+				.and()
+				.exceptionHandling()
+				.and()
+				.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		
+		http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+	}
+}
