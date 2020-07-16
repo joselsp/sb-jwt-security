83a75859c7f20d94fbc0b164696707bc86247193
Jose Luis Solis <solis.jl@gmail.com>
2020-07-16 03:37:55 -0700

    h2 console enable, user entity and repository created, init app data method created

--- src/main/java/com/keepcoding/jwtsecurity/JwtSecurityApplication.java
+++ src/main/java/com/keepcoding/jwtsecurity/JwtSecurityApplication.java
@@ -1,13 +1,33 @@
 package com.keepcoding.jwtsecurity;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.keepcoding.jwtsecurity.model.User;
+import com.keepcoding.jwtsecurity.repository.UserRepository;
+
 @SpringBootApplication
 public class JwtSecurityApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(JwtSecurityApplication.class, args);
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public void initUsers() {
+		List<User> users = Stream.of(
+				new User(101, "dns", "password", "dns@gmail.com"),
+				new User(102, "jorge", "password", "jorge@gmail.com"),
+				new User(103, "pedro", "password", "pedro@gmail.com"),
+				new User(104, "antonio", "password", "antonio@gmail.com")
+		).collect(Collectors.toList());
+		
+		userRepository.saveAll(users);
 	}
+
+	public static void main(String[] args) { SpringApplication.run(JwtSecurityApplication.class, args);	}
 
 }
---
+++ src/main/java/com/keepcoding/jwtsecurity/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.keepcoding.jwtsecurity.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.keepcoding.jwtsecurity.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer>{
+
+	User findByName(String name);
+}
